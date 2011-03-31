ffc763f2ff4ad318ee5716f13714b1d16a2ffb5d
Siddharth Vadgama <siddv@hotmail.com>
2011-03-31 11:17:16 +0100

    done

--- silph.py
+++ silph.py
@@ -7,23 +7,30 @@ class Silph:
 	clearance = 0
 	login1 = False 
 	def list(self):
-		print "\n/docs\n/survailence\n"	
+		print "You've requested idents for silphco.com"
+		print "USERNAME: Silphadmin"	
+		print "PASSWORD: SWJ698"
+		
 	def login(self):
 		print "Please enter your Silph unified access key"
 silph = Silph()
 
-print "Welcome to the Silph Co. mainframe"
+print "Welcome to the Silph Co. mainframe. Type 'help' for a list of commands"
 while(True):
 	userInput = raw_input(">")
-	if userInput == "ssh silph.com":
+	if userInput == "ssh silphco.com":
 		silph.login()
 	elif userInput == "LJ43":
 		silph.login1 = True
 		print "Connection successful"
-	elif userInput == "ls":
+	elif userInput == "getpass":
 		if silph.login1 != True:
-			print "You need to log in to use this command"
+			print "You need to ssh into the server to use this command"
 		else:
 			silph.list()
+	elif userInput == "help":
+		print "ssh <server>"
+		print "getpass"
+		print "quit"
 	elif userInput == "quit":
 		quit()
